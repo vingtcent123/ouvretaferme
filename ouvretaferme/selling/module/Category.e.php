c7920b5c2d942842c5c71736a2543267f2754e92
Vincent Guth <vincent.guth@gmail.com>
2024-07-02 19:36:59 +0200

    Catégories de produits

---
+++ ouvretaferme/selling/module/Category.e.php
@@ -0,0 +1,16 @@
+<?php
+namespace selling;
+
+class Category extends CategoryElement {
+
+	public function canRead(): bool {
+		return $this->canWrite();
+	}
+
+	public function canWrite(): bool {
+		$this->expects(['farm']);
+		return $this['farm']->canManage();
+	}
+
+}
+?>
