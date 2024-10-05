b95c899a218e8bb74fce0d54b8c308be68aaa49e
Vincent Guth <vincent.guth@gmail.com>
2024-10-05 10:11:31 +0200

    Lien vers les news

--- ouvretaferme/main/view/Main.php
+++ ouvretaferme/main/view/Main.php
@@ -131,6 +131,13 @@ class MainTemplate extends BaseTemplate {
 
 		}
 
+		if(currentDate() <= '2024-10-10') {
+			$h .= '<a href="https://blog.ouvretaferme.org/" class="nav-news" target="_blank">';
+				$h .= '<div class="nav-news-title">'.Asset::icon('cursor-fill').' '.s("Nouveautés").'</div>';
+				$h .= '<div class="nav-news-name">'.\L::getVariable('siteName').'</div>';
+			$h .= '</a>';
+		}
+
 		$h .= '<ul class="nav-actions">';
 
 		if($this->data->userDeletedAt) {
