cc9b2195d14768cba6eb09265733f56a4e9e7392
Vincent Guth <vincent.guth@gmail.com>
2025-06-03 17:02:38 +0200

    fix

--- ouvretaferme/selling/module/Sale.e.php
+++ ouvretaferme/selling/module/Sale.e.php
@@ -360,7 +360,7 @@ class Sale extends SaleElement {
 			$this->isComposition() === FALSE and
 			(
 				$this['shopDate']->empty() or
-				$this['createdBy']->is(\user\ConnectionLib::getOnline()) === FALSE or
+				$this['customer']['user']->is(\user\ConnectionLib::getOnline()) === FALSE or
 				$this['shopDate']['isOrderable'] === FALSE
 			)
 		);
