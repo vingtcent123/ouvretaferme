7f28c0720ed1e8beb0680ef0c16d8081e98e4126
Vincent Guth <vincent.guth@gmail.com>
2025-06-03 16:41:31 +0200

    fix

--- ouvretaferme/selling/ui/Sale.u.php
+++ ouvretaferme/selling/ui/Sale.u.php
@@ -877,6 +877,7 @@ class SaleUi {
 
 		if(
 			$eSale['shopDate']->notEmpty() and
+			$eSale['createdBy']->is(\user\ConnectionLib::getOnline()) and
 			$eSale['shopDate']->acceptOrder()
 		) {
 			return '<span class="btn btn-readonly '.$btn.' sale-preparation-status-'.$eSale['preparationStatus'].'-button" title="'.s("Il sera possible de modifier le statut lorsque la période de prise des commandes sera close.").'">'.self::p('preparationStatus')->values[$eSale['preparationStatus']].'  '.\Asset::icon('lock-fill').'</span>';
