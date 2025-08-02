311f42c3fbedafb05a3c52b72cb9b9beffb208f1
Vincent Guth <vincent.guth@gmail.com>
2025-08-02 22:37:00 +0200

    Mise en cohérence

--- commercialisation/selling/ui/Customer.u.php
+++ commercialisation/selling/ui/Customer.u.php
@@ -522,7 +522,7 @@ class CustomerUi {
 					$h .= new \selling\SaleUi()->getList($eCustomer['farm'], $cSale, hide: ['customer'], cPaymentMethod: $cPaymentMethod);
 
 					if($cSale->empty()) {
-						$h .= '<a href="/selling/sale:create?farm='.$eCustomer['farm']['id'].'&customer='.$eCustomer['id'].'" class="btn btn-outline-secondary">'.s("Créer une première vente").'</a>';
+						$h .= '<a href="/selling/sale:create?farm='.$eCustomer['farm']['id'].'&customer='.$eCustomer['id'].'" class="btn btn-primary btn-lg">'.s("Créer une première vente").'</a>';
 					}
 
 				$h .= '</div>';
