9d02470c77d770a5cfd53218bcccfd67700e33e2
Vincent Guth <vincent.guth@gmail.com>
2025-09-04 13:35:39 +0200

    Adhésion via IBAN

--- ouvretaferme/association/ui/Membership.u.php
+++ ouvretaferme/association/ui/Membership.u.php
@@ -168,6 +168,10 @@ class MembershipUi {
 					$h .= s("Vous pouvez choisir le montant de votre adhésion, le montant minimum pour une année civile étant de <b>{amount}</b>. Le règlement s'effectue par un paiement en ligne avec {icon} Stripe après sélection du montant et acceptation des statuts et du règlement intérieur.", ['icon' => \Asset::icon('stripe'), 'amount' => \util\TextUi::money($fee, precision: 0)]);
 				$h .= '</p>';
 
+				$h .= '<p>';
+					$h .= s("Si vous le souhaitez, plutôt qu'utiliser le paiement par carte bancaire, vous pouvez également faire un virement sur le compte bancaire de l'association en indiquant dans le motif du virement <b>Adhésion ferme {farm}</b> (<link>télécharger l'IBAN</link>).", ['farm' => $eFarm['id'], 'link' => '<a href="'.\Asset::getPath('association', 'document/iban.pdf').'" data-ajax-navigation="never">']);
+				$h .= '</p>';
+
 				$h .= $this->getAmountBlocks($form, [$fee, $fee + 20, $fee + 40], $fee);
 
 				$h .= $form->checkbox('terms', 'yes', [
