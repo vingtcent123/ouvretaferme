6da864003d6367b2b771aa790ba8561d9f654c93
Vincent Guth <vincent.guth@gmail.com>
2025-10-12 17:17:08 +0200

    Typo

--- commercialisation/selling/ui/Product.u.php
+++ commercialisation/selling/ui/Product.u.php
@@ -1227,7 +1227,7 @@ class ProductUi {
 				$d->values = [
 					Product::UNPROCESSED_PLANT => s("Produit brut d'origine végétale").'  <span class="color-muted"><small>'.s("Fruits, légumes, fleurs, plants...").'</small></span>',
 					Product::UNPROCESSED_ANIMAL => s("Produit brut d'origine animale").'  <span class="color-muted"><small>'.s("Viandes, oeufs, animaux vivants...").'</small></span>',
-					Product::PROCESSED_FOOD => s("Produit alimentaire transformé").'  <span class="color-muted"><small>'.s("Pain, charcuteries, boissons, confitures, ...").'</small></span>',
+					Product::PROCESSED_FOOD => s("Produit alimentaire transformé").'  <span class="color-muted"><small>'.s("Pains, charcuteries, boissons, confitures, ...").'</small></span>',
 					Product::PROCESSED_PRODUCT => s("Hygiène, santé, entretien ou cosmétique").'  <span class="color-muted"><small>'.s("Savons, lessives, ...").'</small></span>',
 				];
 				break;
