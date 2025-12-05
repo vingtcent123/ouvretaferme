a139f004b13d578f0f286270edf233059b96bbdc
Vincent Guth <vincent.guth@gmail.com>
2025-12-05 21:42:46 +0100

    Éviter des erreurs

--- ouvretaferme/main/page/configure/migrate.p.php
+++ ouvretaferme/main/page/configure/migrate.p.php
@@ -3,19 +3,11 @@ new Page()
 	->cli('index', function($data) {
 
 		$c = \payment\StripeFarm::model()
-			->select([
-				'id',
-				'aSK' => new Sql('apiSecretKey'),
-				'wSK' => new Sql('webhookSecretKey'),
-			])
+			->select()
 			->getCollection();
 
 		foreach($c as $e) {
 
-			\payment\StripeFarm::model()->update($e, [
-				'apiSecretKey' => $e['aSK'],
-				'webhookSecretKey' => $e['wSK'],
-			]);
 
 		}
 
