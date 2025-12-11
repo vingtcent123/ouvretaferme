8637cffa80b6575a7a459b5c9f89b1325a804c71
Vincent Guth <vincent.guth@gmail.com>
2025-12-11 01:58:55 +0100

    Ajout des dates d'échéance sur les factures

--- commercialisation/selling/module/Invoice.m.php
+++ commercialisation/selling/module/Invoice.m.php
@@ -65,6 +65,7 @@ class InvoiceModel extends \ModuleModel {
 			'priceExcludingVat' => ['decimal', 'digits' => 8, 'decimal' => 2, 'cast' => 'float'],
 			'priceIncludingVat' => ['decimal', 'digits' => 8, 'decimal' => 2, 'cast' => 'float'],
 			'date' => ['date', 'cast' => 'string'],
+			'dueDate' => ['date', 'null' => TRUE, 'cast' => 'string'],
 			'paymentMethod' => ['element32', 'payment\Method', 'null' => TRUE, 'cast' => 'element'],
 			'paymentStatus' => ['enum', [\selling\Invoice::PAID, \selling\Invoice::NOT_PAID], 'cast' => 'enum'],
 			'paymentCondition' => ['editor16', 'min' => 1, 'max' => 400, 'null' => TRUE, 'cast' => 'string'],
@@ -81,7 +82,7 @@ class InvoiceModel extends \ModuleModel {
 		]);
 
 		$this->propertiesList = array_merge($this->propertiesList, [
-			'id', 'document', 'name', 'customer', 'sales', 'taxes', 'organic', 'conversion', 'description', 'content', 'farm', 'hasVat', 'vatByRate', 'vat', 'priceExcludingVat', 'priceIncludingVat', 'date', 'paymentMethod', 'paymentStatus', 'paymentCondition', 'header', 'footer', 'generation', 'closed', 'closedAt', 'closedBy', 'accountingHash', 'readyForAccounting', 'emailedAt', 'createdAt'
+			'id', 'document', 'name', 'customer', 'sales', 'taxes', 'organic', 'conversion', 'description', 'content', 'farm', 'hasVat', 'vatByRate', 'vat', 'priceExcludingVat', 'priceIncludingVat', 'date', 'dueDate', 'paymentMethod', 'paymentStatus', 'paymentCondition', 'header', 'footer', 'generation', 'closed', 'closedAt', 'closedBy', 'accountingHash', 'readyForAccounting', 'emailedAt', 'createdAt'
 		]);
 
 		$this->propertiesToModule += [
@@ -242,6 +243,10 @@ class InvoiceModel extends \ModuleModel {
 
 	public function whereDate(...$data): InvoiceModel {
 		return $this->where('date', ...$data);
+	}
+
+	public function whereDueDate(...$data): InvoiceModel {
+		return $this->where('dueDate', ...$data);
 	}
 
 	public function wherePaymentMethod(...$data): InvoiceModel {
