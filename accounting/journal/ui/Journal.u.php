517dfe604797ab2f1871098199f135b3fbc18bd2
Émilie Guth <emilie.guth@gmail.com>
2026-01-05 20:58:55 +0100

    oups

--- accounting/journal/ui/Journal.u.php
+++ accounting/journal/ui/Journal.u.php
@@ -509,7 +509,6 @@ class JournalUi {
 									$h .= '<div class="dropdown-list">';
 										$h .= '<div class="dropdown-title">'.new OperationUi()->getTitle($eOperation).'</div>';
 										$h .= '<a href="'.\company\CompanyUi::urlJournal($eFarm).'/operation/'.$eOperation['id'].'?'.http_build_query($args).'" class="dropdown-item" data-view-operation="'.$eOperation['id'].'">'.s("Voir l'écriture").'</a>';
-										$h .= '<a href="'.\company\CompanyUi::urlJournal($eFarm).'/operation/'.$eOperation['id'].'?'.http_build_query($args).'" class="dropdown-item" data-view-operation="'.$eOperation['id'].'">'.s("Voir l'écriture").'</a>';
 
 										if($eOperation->canUpdate() and $eOperation->acceptUpdate()) {
 
