08df311314e471ab6c2f1636f776417c45bdcf52
Émilie Guth <emilie.guth@gmail.com>
2026-01-10 17:15:25 +0100

    Activation de fermes du 10 janvier

--- accounting/company/Company.s.php
+++ accounting/company/Company.s.php
@@ -9,6 +9,7 @@ class CompanySetting extends \Settings {
 		62, 86, 92, 266, 359, 913, 962, 972, 1160, 1298, 1340, 1396, 1597, 1612, 1954, 2468, 2607, 2736, // 06 janvier
 		70, 322, 527, 1041, 1283, 1756, 1890, 2756, // 08 janvier
 		2154, 1269, 348, 2103, // 09 janvier
+		1664, 2177, 720, 1198, 634 // 10 janvier
 	];
 
 	const CATEGORIE_JURIDIQUE_ENTREPRENEUR_INDIVIDUEL = 1000;
