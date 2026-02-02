9483de3d283257b8c818578834f8a0c3e29edb2c
Émilie Guth <emilie.guth@gmail.com>
2026-02-02 18:05:22 +0100

    :lipstick: Alignement débit / crédit écritures de TVA

--- accounting/overview/ui/Vat.u.php
+++ accounting/overview/ui/Vat.u.php
@@ -5202,7 +5202,7 @@ Class VatUi {
 							$h .= '<div class="journal-operation-description" data-dropdown="bottom" data-dropdown-hover="true">'.encode($eOperation['accountLabel']).'</div>';
 							$h .= '<div class="dropdown-list bg-primary"><span class="dropdown-item">'.encode($eOperation['account']['description']).'</span></div>';
 						$h .= '</td>';
-						$h .= '<td><div class="description"><span class=" ml-3">'.encode($eOperation['description']).'</span></div></td>';
+						$h .= '<td><div class="description"><span class="'.($eOperation['type'] === \journal\Operation::CREDIT ? 'ml-3' : '').'">'.encode($eOperation['description']).'</span></div></td>';
 						$h .= '<td>'.encode($eOperation['thirdParty']['name'] ?? '').'</td>';
 						$h .= '<td class="text-end highlight-stick-right td-vertical-align-top">'.($eOperation['type'] === \journal\Operation::DEBIT ? \util\TextUi::money($eOperation['amount']) : '').'</td>';
 						$h .= '<td class="text-end highlight-stick-left td-vertical-align-top">'.($eOperation['type'] === \journal\Operation::CREDIT ? \util\TextUi::money($eOperation['amount']) : '').'</td>';
