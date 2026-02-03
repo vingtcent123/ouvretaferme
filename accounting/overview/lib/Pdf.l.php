afc0d4fdfc41ef88f5c3a997189fc5791d5a5693
Émilie Guth <emilie.guth@gmail.com>
2026-02-03 16:24:29 +0100

    Bugfix génération de PDF suite à changement d'URLs

--- accounting/overview/lib/Pdf.l.php
+++ accounting/overview/lib/Pdf.l.php
@@ -114,7 +114,7 @@ Class PdfLib {
 		$footer = \account\PdfUi::getFooter();
 		$title = new \account\PdfUi()->getName($eFinancialYear, $type);
 
-		return self::build(\Lime::getUrl().\farm\FarmUi::urlConnected($eFarm).$document.'?type='.$type, $title, $footer);
+		return self::build(\Lime::getUrl().\farm\FarmUi::urlFinancialYear($eFinancialYear, $eFarm).$document.'?type='.$type, $title, $footer);
 
 	}
 }
