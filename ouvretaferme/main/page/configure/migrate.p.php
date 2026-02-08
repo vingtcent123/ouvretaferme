9cee0b59ace02036c493c9675fa0db3ef83f0a3b
Vincent Guth <vincent.guth@gmail.com>
2026-02-08 15:55:25 +0100

    Migration des ventes

--- ouvretaferme/main/page/configure/migrate.p.php
+++ ouvretaferme/main/page/configure/migrate.p.php
@@ -2,6 +2,68 @@
 new Page()
 	->cli('index', function($data) {
 
+		$c = \selling\Sale::model()
+			->select('id', 'paymentStatus')
+			->whereFarm(7)
+			->getCollection();
+
+		foreach($c as $e) {
+
+			if(
+				$e['paymentStatus'] === NULL and
+				\selling\Payment::model()
+					->whereSale($e)
+					->exists() === FALSE
+			) {
+				continue;
+			}
+
+
+			if($e['paymentStatus'] === \selling\Sale::NOT_PAID) {
+
+				if(
+					\selling\Payment::model()
+						->whereSale($e)
+						->whereStatus(\selling\Payment::NOT_PAID)
+						->exists() and
+					\selling\Payment::model()
+						->whereSale($e)
+						->whereStatus('!=', \selling\Payment::NOT_PAID)
+						->exists() === FALSE
+				) {
+					continue;
+				} else {
+					echo '!';
+					continue;
+				}
+
+			}
+
+			if(
+				$e['paymentStatus'] === \selling\Sale::PAID and
+				\selling\Payment::model()
+					->whereStatus(\selling\Payment::PAID)
+					->whereSale($e)
+					->exists()
+			) {
+
+				\selling\Sale::model()
+					->select(\selling\Sale::getSelection())
+					->get($e);
+
+				\selling\PaymentTransactionLib::recalculate($e);
+			} else {
+				echo $e['id'];
+			}
+
+			echo '.';
+
+		}
+
+	});
+	/*
+	->cli('index', function($data) {
+
 		$c = \selling\Customer::model()
 			->select('id', 'farm')
 			->whereNumber(NULL)
@@ -18,5 +80,5 @@ new Page()
 
 		}
 
-	});
+	});*/
 ?>
