a2ae664fa3ec2b4c88dd30783b836b239ea625be
Émilie Guth <emilie.guth@gmail.com>
2026-02-23 16:34:47 +0100

    Clean indentation

--- commercialisation/selling/lib/Ubl.l.php
+++ commercialisation/selling/lib/Ubl.l.php
@@ -37,140 +37,140 @@ class UblLib {
 
 		$xml = '<?xml version="1.0" encoding="utf-8" ?>
 <Invoice xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2"
-    xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
-    xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xmlns:ccts="urn:un:unece:uncefact:documentation:2"
-    xmlns:qdt="urn:oasis:names:specification:ubl:schema:xsd:QualifiedDatatypes-2"
-    xmlns:udt="urn:oasis:names:specification:ubl:schema:xsd:UnqualifiedDataTypes-2"><!--BG-2-->
-    <cbc:UBLVersionID>2.1</cbc:UBLVersionID>
-    <cbc:CustomizationID>urn:cen.eu:en16931:2017</cbc:CustomizationID><!--BT-24-->
-    <cbc:ProfileID>S1</cbc:ProfileID><!--BT-23-->
-    <cbc:ID>'.encode($eInvoice['number']).'</cbc:ID><!--BT-1-->
-    <cbc:IssueDate>'.$eInvoice['date'].'</cbc:IssueDate><!--BT-2-->
-    <cbc:DueDate>'.$eInvoice['dueDate'].'</cbc:DueDate><!--BT-9-->
-    <cbc:InvoiceTypeCode>'.self::getInvoiceTypeCode($eInvoice).'</cbc:InvoiceTypeCode><!--BT-3-->
-    <cbc:Note>#PMD#Tout retard de paiement engendre une pénalité exigible à compter de la date d\'échéance, calculée sur la base de trois fois le taux d\'intérêt légal.</cbc:Note><!--BT-21 PMD (TODO)-->
-    <cbc:Note>#PMT#Indemnité forfaitaire pour frais de recouvrement en cas de retard de paiement : 40 €.</cbc:Note><!--BT-21 PMT (TODO)-->
-    <cbc:Note>#AAB#Les règlements reçus avant la date d\'échéance ne donneront pas lieu à escompte.</cbc:Note><!--BT-21 AAB (TODO)-->
-    <cbc:DocumentCurrencyCode>EUR</cbc:DocumentCurrencyCode><!--BT-5-->
-    <cac:InvoicePeriod><!--BG-14-->
-        <cbc:StartDate>'.$dateMin.'</cbc:StartDate><!--BT-73-->
-        <cbc:EndDate>'.$dateMax.'</cbc:EndDate><!--BT-74-->
-        <cbc:DescriptionCode>'.self::getVatChargeability($eFarm, $eInvoice).'</cbc:DescriptionCode><!--BT-8-->
-    </cac:InvoicePeriod>
-    <cac:OrderReference>
-        <cbc:ID>PO202525478</cbc:ID><!--BT-13 (TODO)-->
-    </cac:OrderReference>
-    <cac:AccountingSupplierParty><!--BG-4-->
-        <cac:Party>
-            <cbc:EndpointID schemeID="0225">'.$sellerAddress.'</cbc:EndpointID><!--BT-34-->
-            <cac:PartyIdentification>
-                <cbc:ID schemeID="0088">'.$sellerSiret.'</cbc:ID><!--BT-29-->
-            </cac:PartyIdentification>
-            <cac:PartyName>
-                <cbc:Name>'.$eFarm['name'].'</cbc:Name><!--BT-28-->
-            </cac:PartyName>
-            <cac:PostalAddress><!--BG-5-->
-                <cbc:StreetName>'.$eFarm['legalStreet1'].'</cbc:StreetName>
-                <cbc:AdditionalStreetName>'.$eFarm['legalStreet2'].'</cbc:AdditionalStreetName>
-                <cbc:CityName>'.$eFarm['legalCity'].'</cbc:CityName>
-                <cbc:PostalZone>'.$eFarm['legalPostcode'].'</cbc:PostalZone>
-                <cac:Country>
-                    <cbc:IdentificationCode>'.$eFarm['legalCountry']['code'].'</cbc:IdentificationCode><!--BT-40-->
-                </cac:Country>
-            </cac:PostalAddress>
-            <cac:PartyTaxScheme>
-                <cbc:CompanyID>'.$eFarm->getConf('vatNumber').'</cbc:CompanyID><!--BT-31-->
-                <cac:TaxScheme>
-                    <cbc:ID>VAT</cbc:ID>
-                </cac:TaxScheme>
-            </cac:PartyTaxScheme>
-            <cac:PartyLegalEntity>
-                <cbc:RegistrationName>'.$eFarm['legalName'].'</cbc:RegistrationName><!--BT-27-->
-                <cbc:CompanyID schemeID="0002">'.$sellerSiren.'</cbc:CompanyID><!--BT-30-->
-            </cac:PartyLegalEntity>
-        </cac:Party>
-    </cac:AccountingSupplierParty>
-    <cac:AccountingCustomerParty><!--BG-7-->
-        <cac:Party>
-            <cbc:EndpointID schemeID="0225">'.$buyerAddress.'</cbc:EndpointID><!--BT-49-->
-            <cac:PartyIdentification>
-                <cbc:ID schemeID="0088">'.$buyerSiret.'</cbc:ID><!--BT-46-->
-            </cac:PartyIdentification>
-            <cac:PostalAddress><!--BG-8-->
-                <cbc:StreetName>'.$eInvoice['customer']['invoiceStreet1'].'</cbc:StreetName>
-                <cbc:AdditionalStreetName>'.$eInvoice['customer']['invoiceStreet2'].'</cbc:AdditionalStreetName>
-                <cbc:CityName>'.$eInvoice['customer']['invoiceCity'].'</cbc:CityName>
-                <cbc:PostalZone>'.$eInvoice['customer']['invoicePostcode'].'</cbc:PostalZone>
-                <cac:Country>
-                    <cbc:IdentificationCode>'.$eInvoice['customer']['invoiceCountry']['code'].'</cbc:IdentificationCode>
-                </cac:Country>
-            </cac:PostalAddress>
-            <cac:PartyTaxScheme>
-                <cbc:CompanyID>'.$eInvoice['customer']['vatNumber'].'</cbc:CompanyID><!--BT-48-->
-                <cac:TaxScheme>
-                    <cbc:ID>VAT</cbc:ID>
-                </cac:TaxScheme>
-            </cac:PartyTaxScheme>
-            <cac:PartyLegalEntity>
-                <cbc:RegistrationName>'.$eInvoice['customer']['legalName'].'</cbc:RegistrationName><!--BT-44-->
-                <cbc:CompanyID schemeID="0002">'.$buyerSiren.'</cbc:CompanyID><!--BT-47-->
-            </cac:PartyLegalEntity>
-        </cac:Party>
-    </cac:AccountingCustomerParty>
-    <cac:TaxTotal><!--BT-110-00-->
-        <cbc:TaxAmount currencyID="EUR">'.$eInvoice['vat'].'</cbc:TaxAmount><!--BT-110-->';
-				foreach($eInvoice['vatByRate'] as $vatByRate) {
+	xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
+	xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xmlns:ccts="urn:un:unece:uncefact:documentation:2"
+	xmlns:qdt="urn:oasis:names:specification:ubl:schema:xsd:QualifiedDatatypes-2"
+	xmlns:udt="urn:oasis:names:specification:ubl:schema:xsd:UnqualifiedDataTypes-2"><!--BG-2-->
+	<cbc:UBLVersionID>2.1</cbc:UBLVersionID>
+	<cbc:CustomizationID>urn:cen.eu:en16931:2017</cbc:CustomizationID><!--BT-24-->
+	<cbc:ProfileID>S1</cbc:ProfileID><!--BT-23-->
+	<cbc:ID>'.encode($eInvoice['number']).'</cbc:ID><!--BT-1-->
+	<cbc:IssueDate>'.$eInvoice['date'].'</cbc:IssueDate><!--BT-2-->
+	<cbc:DueDate>'.$eInvoice['dueDate'].'</cbc:DueDate><!--BT-9-->
+	<cbc:InvoiceTypeCode>'.self::getInvoiceTypeCode($eInvoice).'</cbc:InvoiceTypeCode><!--BT-3-->
+	<cbc:Note>#PMD#Tout retard de paiement engendre une pénalité exigible à compter de la date d\'échéance, calculée sur la base de trois fois le taux d\'intérêt légal.</cbc:Note><!--BT-21 PMD (TODO)-->
+	<cbc:Note>#PMT#Indemnité forfaitaire pour frais de recouvrement en cas de retard de paiement : 40 €.</cbc:Note><!--BT-21 PMT (TODO)-->
+	<cbc:Note>#AAB#Les règlements reçus avant la date d\'échéance ne donneront pas lieu à escompte.</cbc:Note><!--BT-21 AAB (TODO)-->
+	<cbc:DocumentCurrencyCode>EUR</cbc:DocumentCurrencyCode><!--BT-5-->
+	<cac:InvoicePeriod><!--BG-14-->
+		<cbc:StartDate>'.$dateMin.'</cbc:StartDate><!--BT-73-->
+		<cbc:EndDate>'.$dateMax.'</cbc:EndDate><!--BT-74-->
+		<cbc:DescriptionCode>'.self::getVatChargeability($eFarm, $eInvoice).'</cbc:DescriptionCode><!--BT-8-->
+	</cac:InvoicePeriod>
+	<cac:OrderReference>
+		<cbc:ID>PO202525478</cbc:ID><!--BT-13 (TODO)-->
+	</cac:OrderReference>
+	<cac:AccountingSupplierParty><!--BG-4-->
+		<cac:Party>
+			<cbc:EndpointID schemeID="0225">'.$sellerAddress.'</cbc:EndpointID><!--BT-34-->
+			<cac:PartyIdentification>
+				<cbc:ID schemeID="0088">'.$sellerSiret.'</cbc:ID><!--BT-29-->
+			</cac:PartyIdentification>
+			<cac:PartyName>
+				<cbc:Name>'.$eFarm['name'].'</cbc:Name><!--BT-28-->
+			</cac:PartyName>
+			<cac:PostalAddress><!--BG-5-->
+				<cbc:StreetName>'.$eFarm['legalStreet1'].'</cbc:StreetName>
+				<cbc:AdditionalStreetName>'.$eFarm['legalStreet2'].'</cbc:AdditionalStreetName>
+				<cbc:CityName>'.$eFarm['legalCity'].'</cbc:CityName>
+				<cbc:PostalZone>'.$eFarm['legalPostcode'].'</cbc:PostalZone>
+				<cac:Country>
+					<cbc:IdentificationCode>'.$eFarm['legalCountry']['code'].'</cbc:IdentificationCode><!--BT-40-->
+				</cac:Country>
+			</cac:PostalAddress>
+			<cac:PartyTaxScheme>
+				<cbc:CompanyID>'.$eFarm->getConf('vatNumber').'</cbc:CompanyID><!--BT-31-->
+				<cac:TaxScheme>
+					<cbc:ID>VAT</cbc:ID>
+				</cac:TaxScheme>
+			</cac:PartyTaxScheme>
+			<cac:PartyLegalEntity>
+				<cbc:RegistrationName>'.$eFarm['legalName'].'</cbc:RegistrationName><!--BT-27-->
+				<cbc:CompanyID schemeID="0002">'.$sellerSiren.'</cbc:CompanyID><!--BT-30-->
+			</cac:PartyLegalEntity>
+		</cac:Party>
+	</cac:AccountingSupplierParty>
+	<cac:AccountingCustomerParty><!--BG-7-->
+		<cac:Party>
+			<cbc:EndpointID schemeID="0225">'.$buyerAddress.'</cbc:EndpointID><!--BT-49-->
+			<cac:PartyIdentification>
+				<cbc:ID schemeID="0088">'.$buyerSiret.'</cbc:ID><!--BT-46-->
+			</cac:PartyIdentification>
+			<cac:PostalAddress><!--BG-8-->
+				<cbc:StreetName>'.$eInvoice['customer']['invoiceStreet1'].'</cbc:StreetName>
+				<cbc:AdditionalStreetName>'.$eInvoice['customer']['invoiceStreet2'].'</cbc:AdditionalStreetName>
+				<cbc:CityName>'.$eInvoice['customer']['invoiceCity'].'</cbc:CityName>
+				<cbc:PostalZone>'.$eInvoice['customer']['invoicePostcode'].'</cbc:PostalZone>
+				<cac:Country>
+					<cbc:IdentificationCode>'.$eInvoice['customer']['invoiceCountry']['code'].'</cbc:IdentificationCode>
+				</cac:Country>
+			</cac:PostalAddress>
+			<cac:PartyTaxScheme>
+				<cbc:CompanyID>'.$eInvoice['customer']['vatNumber'].'</cbc:CompanyID><!--BT-48-->
+				<cac:TaxScheme>
+					<cbc:ID>VAT</cbc:ID>
+				</cac:TaxScheme>
+			</cac:PartyTaxScheme>
+			<cac:PartyLegalEntity>
+				<cbc:RegistrationName>'.$eInvoice['customer']['legalName'].'</cbc:RegistrationName><!--BT-44-->
+				<cbc:CompanyID schemeID="0002">'.$buyerSiren.'</cbc:CompanyID><!--BT-47-->
+			</cac:PartyLegalEntity>
+		</cac:Party>
+	</cac:AccountingCustomerParty>
+	<cac:TaxTotal><!--BT-110-00-->
+		<cbc:TaxAmount currencyID="EUR">'.$eInvoice['vat'].'</cbc:TaxAmount><!--BT-110-->';
+			foreach($eInvoice['vatByRate'] as $vatByRate) {
         $xml .= '
-          <cac:TaxSubtotal><!--BG-23-->
-            <cbc:TaxableAmount currencyID="EUR">'.$vatByRate['amount'].'</cbc:TaxableAmount>
-            <cbc:TaxAmount currencyID="EUR">'.$vatByRate['vat'].'</cbc:TaxAmount>
-            <cac:TaxCategory>
-                <cbc:ID>'.self::getVatCode($vatByRate).'</cbc:ID><!--BT-118-->
-                <cbc:Percent>'.$vatByRate['vatRate'].'</cbc:Percent><!--BT-119-->
-                <cac:TaxScheme>
-                    <cbc:ID>VAT</cbc:ID>
-                </cac:TaxScheme>
-            </cac:TaxCategory>
-        </cac:TaxSubtotal>';
+		<cac:TaxSubtotal><!--BG-23-->
+			<cbc:TaxableAmount currencyID="EUR">'.$vatByRate['amount'].'</cbc:TaxableAmount>
+			<cbc:TaxAmount currencyID="EUR">'.$vatByRate['vat'].'</cbc:TaxAmount>
+			<cac:TaxCategory>
+				<cbc:ID>'.self::getVatCode($vatByRate).'</cbc:ID><!--BT-118-->
+				<cbc:Percent>'.$vatByRate['vatRate'].'</cbc:Percent><!--BT-119-->
+				<cac:TaxScheme>
+					<cbc:ID>VAT</cbc:ID>
+				</cac:TaxScheme>
+			</cac:TaxCategory>
+		</cac:TaxSubtotal>';
 				}
 		$xml .= '
-    </cac:TaxTotal>
-    <cac:LegalMonetaryTotal>
-        <cbc:LineExtensionAmount currencyID="EUR">'.self::sumPriceExcludingVat($eInvoice).'</cbc:LineExtensionAmount><!--BT-106-->
-        <cbc:TaxExclusiveAmount currencyID="EUR">'.$eInvoice['priceExcludingVat'].'</cbc:TaxExclusiveAmount><!--BT-109-->
-        <cbc:TaxInclusiveAmount currencyID="EUR">'.$eInvoice['priceIncludingVat'].'</cbc:TaxInclusiveAmount><!--BT-112-->
-        <cbc:PayableAmount currencyID="EUR">'.$eInvoice['priceIncludingVat'].'</cbc:PayableAmount><!--BT-115-->
-    </cac:LegalMonetaryTotal>';
-		$lineNumber = 1;
-		foreach($eInvoice['cSale'] as $eSale) {
-			foreach($eSale['cItem'] as $eItem) {
-        $xml .= '
-    <cac:InvoiceLine><!--BG-25-->
-        <cbc:ID>'.$lineNumber.'</cbc:ID><!--BT-126-->
-        <cbc:InvoicedQuantity unitCode="'.self::getUnitCode($eItem['unit']).'">'.$eItem['number'].'</cbc:InvoicedQuantity><!--BT-129--><!--BT-130-->
-        <cbc:LineExtensionAmount currencyID="EUR">'.$eItem['price'].'</cbc:LineExtensionAmount><!--BT-131-->
-        <cac:InvoicePeriod><!--BG-26-->
-          <cbc:StartDate>'.$eSale['deliveredAt'].'</cbc:StartDate><!--BT-134-->
-          <cbc:EndDate>'.$eSale['deliveredAt'].'</cbc:EndDate><!--BT-135-->
-        </cac:InvoicePeriod>
-        <cac:Item>
-            <cbc:Name>'.$eItem['name'].'</cbc:Name><!--BT-153-->
-            <cac:ClassifiedTaxCategory><!--BG-30-->
-                <cbc:ID>'.self::getVatCode($eItem['vatRate']).'</cbc:ID>
-                <cbc:Percent>'.$eItem['vatRate'].'</cbc:Percent>
-                <cac:TaxScheme>
-                    <cbc:ID>VAT</cbc:ID>
-                </cac:TaxScheme>
-            </cac:ClassifiedTaxCategory>
-        </cac:Item>
-        <cac:Price><!--BG-29-->
-            <cbc:PriceAmount currencyID="EUR">'.$eItem['unitPrice'].'</cbc:PriceAmount><!--BT-146-->
-            <cbc:BaseQuantity unitCode="'.self::getUnitCode($eItem['unit']).'">'.$eItem['number'].'</cbc:BaseQuantity>';
-        $xml .= '
-        </cac:Price>
-    </cac:InvoiceLine>';
+	</cac:TaxTotal>
+	<cac:LegalMonetaryTotal>
+		<cbc:LineExtensionAmount currencyID="EUR">'.self::sumPriceExcludingVat($eInvoice).'</cbc:LineExtensionAmount><!--BT-106-->
+		<cbc:TaxExclusiveAmount currencyID="EUR">'.$eInvoice['priceExcludingVat'].'</cbc:TaxExclusiveAmount><!--BT-109-->
+		<cbc:TaxInclusiveAmount currencyID="EUR">'.$eInvoice['priceIncludingVat'].'</cbc:TaxInclusiveAmount><!--BT-112-->
+		<cbc:PayableAmount currencyID="EUR">'.$eInvoice['priceIncludingVat'].'</cbc:PayableAmount><!--BT-115-->
+	</cac:LegalMonetaryTotal>';
+	$lineNumber = 1;
+	foreach($eInvoice['cSale'] as $eSale) {
+		foreach($eSale['cItem'] as $eItem) {
+      $xml .= '
+	<cac:InvoiceLine><!--BG-25-->
+		<cbc:ID>'.$lineNumber.'</cbc:ID><!--BT-126-->
+		<cbc:InvoicedQuantity unitCode="'.self::getUnitCode($eItem['unit']).'">'.$eItem['number'].'</cbc:InvoicedQuantity><!--BT-129--><!--BT-130-->
+		<cbc:LineExtensionAmount currencyID="EUR">'.$eItem['price'].'</cbc:LineExtensionAmount><!--BT-131-->
+		<cac:InvoicePeriod><!--BG-26-->
+			<cbc:StartDate>'.$eSale['deliveredAt'].'</cbc:StartDate><!--BT-134-->
+			<cbc:EndDate>'.$eSale['deliveredAt'].'</cbc:EndDate><!--BT-135-->
+		</cac:InvoicePeriod>
+		<cac:Item>
+			<cbc:Name>'.$eItem['name'].'</cbc:Name><!--BT-153-->
+			<cac:ClassifiedTaxCategory><!--BG-30-->
+				<cbc:ID>'.self::getVatCode($eItem['vatRate']).'</cbc:ID>
+				<cbc:Percent>'.$eItem['vatRate'].'</cbc:Percent>
+				<cac:TaxScheme>
+					<cbc:ID>VAT</cbc:ID>
+				</cac:TaxScheme>
+			</cac:ClassifiedTaxCategory>
+		</cac:Item>
+		<cac:Price><!--BG-29-->
+		<cbc:PriceAmount currencyID="EUR">'.$eItem['unitPrice'].'</cbc:PriceAmount><!--BT-146-->
+		<cbc:BaseQuantity unitCode="'.self::getUnitCode($eItem['unit']).'">'.$eItem['number'].'</cbc:BaseQuantity>';
+		$xml .= '
+		</cac:Price>
+	</cac:InvoiceLine>';
 				$lineNumber++;
 			}
 		}
