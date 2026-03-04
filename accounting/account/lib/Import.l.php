f25efbce669523a0236918daeeb5f99b0a7e8236
Émilie Guth <emilie.guth@gmail.com>
2026-03-04 18:56:14 +0100

    :bug: Clean du clean

--- accounting/account/lib/Import.l.php
+++ accounting/account/lib/Import.l.php
@@ -104,42 +104,6 @@ Class ImportLib extends ImportCrud {
 
 	}
 
-	private static function cleanContent(string $string): string {
-
-		$string = trim($string);
-
-		$search = array("&#39;", "\xc3\xa2\xc2\x80\xc2\x99", "\xc3\xa2\xc2\x80\xc2\x93", "\xc3\xa2\xc2\x80\xc2\x9d", "\xc3\xa2\x3f\x3f");
-    $replace = array("'", "'", ' - ', '"', "'");
-
-    $string = str_replace($search, $replace, $string);
-
-		$quotes = array(
-			"\xC2\xAB"     => '"',
-			"\xC2\xBB"     => '"',
-			"\xE2\x80\x98" => "'",
-			"\xE2\x80\x99" => "'",
-			"\xE2\x80\x9A" => "'",
-			"\xE2\x80\x9B" => "'",
-			"\xE2\x80\x9C" => '"',
-			"\xE2\x80\x9D" => '"',
-			"\xE2\x80\x9E" => '"',
-			"\xE2\x80\x9F" => '"',
-			"\xE2\x80\xB9" => "'",
-			"\xE2\x80\xBA" => "'",
-			"\xe2\x80\x93" => "-",
-			"\xc2\xb0"	   => "°",
-			"\xc2\xba"     => "°",
-			"\xc3\xb1"	   => "&#241;",
-			"\x96"		   => "&#241;",
-			"\xe2\x81\x83" => '&bull;'
-		);
-
-		$string = strtr($string, $quotes);
-
-		return $string;
-
-	}
-
 	public static function create(Import $e): void {
 
 		if(isset($_FILES['fec']['tmp_name']) === FALSE or $_FILES['fec']['tmp_name'] === '') {
